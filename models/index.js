1d405caf523f3f8c32e46c8d99aaf281f59107e6
DareEnd <114109917+Darin1027@users.noreply.github.com>
2022-12-15 14:23:53 -0700

    added code to index.js in /models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,37 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.hasMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // Define an alias for when data is retrieved
+  as: "product_tags",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.hasMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // Define an alias for when data is retrieved
+  as: "tag_products",
+});
 
 module.exports = {
   Product,
@@ -21,67 +46,3 @@ module.exports = {
 
 // todo - Getting Started
 //* Use the schema.sql file in the db folder to create your database using MySQL shell commands. Use environment variables to store sensitive data, like your MySQL username, password, and database name.
-
-// todo - Database Models
-// Your database should contain the following four models, including the requirements listed for each model:
-
-//! Category
-//? id
-// Integer
-// Doesn't allow null values
-// Set as primary key
-// Uses auto increment
-//? category_name
-// String
-// Doesn't allow null values
-
-//! Product
-//? id
-// Integer
-// Doesn't allow null values
-// Set as primary key
-// Uses auto increment
-//? product_name
-// String
-// Doesn't allow null values
-//? price
-// Decimal
-// Doesn't allow null values
-// Validates that the value is a decimal
-//? stock
-// Integer
-// Doesn't allow null values
-// Set a default value of 10
-// Validates that the value is numeric
-//? category_id
-// Integer
-// References the category model's id
-
-//! Tag
-//? id
-// Integer
-// Doesn't allow null values
-// Set as primary key
-// Uses auto increment
-//? tag_name
-// String
-
-//! ProductTag
-//? id
-// Integer
-// Doesn't allow null values
-// Set as primary key
-// Uses auto increment
-//? product_id
-// Integer
-// References the product model's id
-//? tag_id
-// Integer
-// References the tag model's id
-
-// todo - Associations
-// You'll need to execute association methods on your Sequelize models to create the following relationships between them:
-// Product belongs to Category, as a category can have multiple products but a product can only belong to one category.
-// Category has many Product models.
-// Product belongs to many Tag models. Using the ProductTag through model, allow products to have multiple tags and tags to have many products.
-// Tag belongs to many Product models.
