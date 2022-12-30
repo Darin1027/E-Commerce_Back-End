87d181f5e49cc314697f607ac2ecfb143bc1516d
DareEnd <114109917+Darin1027@users.noreply.github.com>
2022-12-29 17:52:35 -0700

    saving changes again

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -20,7 +20,7 @@ router.get("/", async (req, res) => {
 // get one product
 // find a single product by its `id`
 // be sure to include its associated Category and Tag data
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {mm
   try {
     const data = await Product.findByPk(
       req.params.id,
